8df555c998b1179fdfe22d683493b1418bfc3a03
katebell483 <kate@giphy.com>
2017-09-17 14:11:42 -0700

    wip

--- picoproapp/src/main/java/com/example/androidthings/myproject/Hw3TemplateApp.java
+++ picoproapp/src/main/java/com/example/androidthings/myproject/Hw3TemplateApp.java
@@ -32,13 +32,13 @@ public class Hw3TemplateApp extends SimplePicoPro {
     SerialMidi serialMidi;
 
     // synth controls
-    int channel = 0;
+    int channel = 1;
     int velocity = 127;
     int timbreValue = 0;
     int prevTimbre;
     final int timbre_controller = 0x47;
     int prevNote;
-    String prevLightState;
+    String prevLightState = "notCovered";
     NavigableMap noteMap;
     NavigableMap timbreMap;
     int noteLength = 2000;
@@ -67,29 +67,36 @@ public class Hw3TemplateApp extends SimplePicoPro {
     public void loop() {
 
         // get analogue readings
-        force = analogRead(A0); // this is pitch
-        light = analogRead(A1); // this is note ON/OFF
-        flex = analogRead(A3); // this is timbre
+        flex = analogRead(A0); // this is timbre
+        force = analogRead(A1); // this is pitch
+        light = analogRead(A2); // this is note ON/OFF
+
         //print("FORCE: " + force);
         //print("LIGHT: " + light);
 
-        delay(300); // is this the right delay?
+        delay(1000); // is this the right delay?
+
+
+        // translate flex sensor input to 0 to 127 range
+        int timbreValue = Math.round(127 * (flex - (float) 2.2));
+        timbreValue = timbreValue > 127 ? 127 : timbreValue;
 
         // do something with the flex
-        /*
-        int timbreValue = (int) timbreMap.floorEntry(flex).getValue();
-        // timbre value goes from 0 to 127
+        //int timbreValue = (int) timbreMap.floorEntry(flex).getValue(); // timbre value goes from 0 to 127
+
+        print("FLEX " + timbreValue);
 
         // check for timbre change
         if(prevTimbre != timbreValue) {
             serialMidi.midi_controller_change(channel, timbre_controller, timbreValue);
             prevTimbre = timbreValue;
         }
-        */
 
         // convert the force sensor value to a note
         int note = (int) noteMap.floorEntry(force).getValue();
 
+        //print("NOTE " + note);
+        //print("FORCE " + force);
         // turn on note for 2s (if diff from previous)
         if(note != prevNote && note != -1) {
             serialMidi.midi_note_on(channel, note, velocity);
@@ -100,30 +107,39 @@ public class Hw3TemplateApp extends SimplePicoPro {
 
         // detect whether light sensor is covered
         String curLightState = light > .5 ? "covered" : "notCovered";
+        print("LIGHT " + light);
+
+        print(curLightState);
+
 
         // if light sensor is covered hold the note and keep track of the note being held
         if(curLightState == "covered" && prevLightState == "notCovered") {
+            //print("HOLDING NOTE " + note);
             noteOffHandler.removeCallbacks(currNoteOffRunnable);
             noteToHold = note;
         }
 
         // if light sensor cover is released then turn off held note
-        if(curLightState == "Notcovered") {
+        if(curLightState == "notCovered" && prevLightState == "covered") {
+            //print("releasing NOTE " + noteToHold);
             Runnable noteOffRunnable = createNoteOffRunnable(noteToHold);
-            noteOffHandler.postDelayed(noteOffRunnable, noteLength);
+            noteOffHandler.postDelayed(noteOffRunnable, 1000);
         }
+
+        // track light changes
+        prevLightState = curLightState;
     }
 
     private NavigableMap<Float, Integer> createNoteMap() {
         NavigableMap<Float, Integer> noteMap = new TreeMap<Float, Integer>();
         noteMap.put((float) 0.0, SerialMidi.MIDI_C4);
-        noteMap.put((float) 0.4, SerialMidi.MIDI_D4);
-        noteMap.put((float) 0.6, SerialMidi.MIDI_E4);
+        noteMap.put((float) 0.3, SerialMidi.MIDI_D4);
+        noteMap.put((float) 0.5, SerialMidi.MIDI_E4);
         noteMap.put((float) 0.8, SerialMidi.MIDI_F4);
         noteMap.put((float) 1.0, SerialMidi.MIDI_G4);
-        noteMap.put((float) 2.0, SerialMidi.MIDI_A5);
-        noteMap.put((float) 3.0, SerialMidi.MIDI_B5);
-        noteMap.put((float) 3.3, -1);
+        noteMap.put((float) 1.3, SerialMidi.MIDI_A5);
+        noteMap.put((float) 1.6, SerialMidi.MIDI_B5);
+        noteMap.put((float) 2.1, -1);
 
         return noteMap;
     }
