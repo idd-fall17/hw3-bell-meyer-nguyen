68c25e7d9a5b9060e074801cd0b49386bfbb5d6c
katebell483 <kate@giphy.com>
2017-09-17 18:16:52 -0700

    wip

--- picoproapp/src/main/java/com/example/androidthings/myproject/Hw3TemplateApp.java
+++ picoproapp/src/main/java/com/example/androidthings/myproject/Hw3TemplateApp.java
@@ -32,17 +32,14 @@ public class Hw3TemplateApp extends SimplePicoPro {
     SerialMidi serialMidi;
 
     // synth controls
-    int channel = 1;
+    int channel = 0;
+    boolean sameNoteNotOK;
     int velocity = 127;
-    int timbreValue = 0;
-    int prevTimbre;
-    final int timbre_controller = 0x47;
     int prevNote;
     String prevLightState = "notCovered";
     NavigableMap noteMap;
-    NavigableMap timbreMap;
     int noteLength = 2000;
-    int noteToHold;
+    int noteBeingHeld;
     Runnable currNoteOffRunnable;
 
     // vars for sensor readings
@@ -60,7 +57,6 @@ public class Hw3TemplateApp extends SimplePicoPro {
 
         // create maps from analogue readings to synth values
         noteMap = createNoteMap();
-        timbreMap = createTimbreMap();
     }
 
     @Override
@@ -71,89 +67,62 @@ public class Hw3TemplateApp extends SimplePicoPro {
         force = analogRead(A1); // this is pitch
         light = analogRead(A2); // this is note ON/OFF
 
-        //print("FORCE: " + force);
-        //print("LIGHT: " + light);
-
-        delay(1000); // is this the right delay?
-
+        delay(300);
 
         // translate flex sensor input to 0 to 127 range
-        int timbreValue = Math.round(127 * (flex - (float) 2.2));
+        int timbreValue = Math.round(127 - 127 * (flex - (float) 2.4));
         timbreValue = timbreValue > 127 ? 127 : timbreValue;
-
-        // do something with the flex
-        //int timbreValue = (int) timbreMap.floorEntry(flex).getValue(); // timbre value goes from 0 to 127
-
-        print("FLEX " + timbreValue);
-
-        // check for timbre change
-        if(prevTimbre != timbreValue) {
-            serialMidi.midi_controller_change(channel, timbre_controller, timbreValue);
-            prevTimbre = timbreValue;
-        }
+        timbreValue = timbreValue < 0 ? 0 : timbreValue;
+        serialMidi.midi_controller_change(channel, 77, timbreValue);
 
         // convert the force sensor value to a note
         int note = (int) noteMap.floorEntry(force).getValue();
 
-        //print("NOTE " + note);
-        //print("FORCE " + force);
-        // turn on note for 2s (if diff from previous)
-        if(note != prevNote && note != -1) {
-            serialMidi.midi_note_on(channel, note, velocity);
-            currNoteOffRunnable = createNoteOffRunnable(note);
-            noteOffHandler.postDelayed(currNoteOffRunnable, noteLength);
-            prevNote = note;
+        // turn on note (if diff from previous)
+        if(note != -1 && note != noteBeingHeld) {
+            if(note == prevNote && sameNoteNotOK || note == prevNote && note != 69) {
+                 // in other words let the first note be played over and over but no other note
+                print("scipping this round");
+            } else {
+                sameNoteNotOK = true;
+                serialMidi.midi_note_on(channel, note, velocity);
+                currNoteOffRunnable = createNoteOffRunnable(note);
+                noteOffHandler.postDelayed(currNoteOffRunnable, noteLength);
+                prevNote = note;
+            }
         }
 
         // detect whether light sensor is covered
         String curLightState = light > .5 ? "covered" : "notCovered";
-        print("LIGHT " + light);
-
-        print(curLightState);
-
 
         // if light sensor is covered hold the note and keep track of the note being held
         if(curLightState == "covered" && prevLightState == "notCovered") {
-            //print("HOLDING NOTE " + note);
+            print("HOLDING NOTE " + note);
             noteOffHandler.removeCallbacks(currNoteOffRunnable);
-            noteToHold = note;
+            noteBeingHeld = note;
         }
 
         // if light sensor cover is released then turn off held note
         if(curLightState == "notCovered" && prevLightState == "covered") {
-            //print("releasing NOTE " + noteToHold);
-            Runnable noteOffRunnable = createNoteOffRunnable(noteToHold);
-            noteOffHandler.postDelayed(noteOffRunnable, 1000);
+            print("releasing NOTE " + noteBeingHeld);
+            Runnable noteOffRunnable = createNoteOffRunnable(noteBeingHeld);
+            noteOffHandler.postDelayed(noteOffRunnable, noteLength);
         }
 
         // track light changes
         prevLightState = curLightState;
     }
 
+    // map ranges to values in a classic (abridged) scale
     private NavigableMap<Float, Integer> createNoteMap() {
         NavigableMap<Float, Integer> noteMap = new TreeMap<Float, Integer>();
         noteMap.put((float) 0.0, SerialMidi.MIDI_C4);
-        noteMap.put((float) 0.3, SerialMidi.MIDI_D4);
-        noteMap.put((float) 0.5, SerialMidi.MIDI_E4);
-        noteMap.put((float) 0.8, SerialMidi.MIDI_F4);
-        noteMap.put((float) 1.0, SerialMidi.MIDI_G4);
-        noteMap.put((float) 1.3, SerialMidi.MIDI_A5);
-        noteMap.put((float) 1.6, SerialMidi.MIDI_B5);
-        noteMap.put((float) 2.1, -1);
-
+        noteMap.put((float) 0.4, SerialMidi.MIDI_E4);
+        noteMap.put((float) 0.6, SerialMidi.MIDI_F4);
+        noteMap.put((float) 0.8, SerialMidi.MIDI_G4);
+        noteMap.put((float) 1.0, SerialMidi.MIDI_A5);
+        noteMap.put((float) 3.3, -1);
         return noteMap;
-    }
-
-    private NavigableMap<Float, Integer> createTimbreMap() {
-        NavigableMap<Float, Integer> timbreMap = new TreeMap<Float, Integer>();
-        timbreMap.put((float) 1.0, 100);
-        timbreMap.put((float) 2.0, 66);
-        timbreMap.put((float) 2.5, 0);
-        timbreMap.put((float) 2.8, 66);
-        timbreMap.put((float) 3.1, 100);
-        timbreMap.put((float) 3.4, 127);
-
-        return timbreMap;
     }
 
     // handler for turning note off
@@ -162,6 +131,7 @@ public class Hw3TemplateApp extends SimplePicoPro {
         Runnable noteOffRunnable = new Runnable(){
             public void run() {
                 serialMidi.midi_note_off(channel, note, velocity);
+                sameNoteNotOK = false;
             };
         };
         return noteOffRunnable;
